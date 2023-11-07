803a68bc0e66107717aae276cc1f6afd071a6be5
Новиков Сергей Александрович <SANovikov@inno.tech>
2023-11-07 16:55:34 +0300

    В pom-нике два способа создания jar 1. Когда зависимости лежат в каталоге /lib в виде jar-ников https://maven.apache.org/plugins/maven-dependency-plugin/usage.html#dependency:copy-dependencies
    2. Это FAT jar, зависимости распакованы в сам jar
    содается с постфиксом "with-dependencies"
    https://maven.apache.org/plugins/maven-assembly-plugin/usage.html

---
+++ src/main/java/Runner.java
@@ -0,0 +1,7 @@
+import com.google.common.base.Strings;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(Strings.padEnd("Hello World",20,'_'));
+    }
+}
